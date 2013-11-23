d7c5ce85386f9ed85edaf2f970294deb96e4bc88
A. Diamond <andrew@7simplemachines.com>
2013-11-23 10:47:05 -0800

    Interim commit. Steady improvements.

--- Presentation/Nop.Web/Scripts/Custom/cyo/cyo.js
+++ Presentation/Nop.Web/Scripts/Custom/cyo/cyo.js
@@ -6,12 +6,19 @@
         $('#cyoSample img').attr('src', $(this).attr('data-large-image'));
     });
 
+    // Load clicked image into pacifier
     $('.chooser img').click(function () {
         var url = $(this).attr('src');
         //$('#cyoCustomPattern').attr('value', url);
         $('#cyoSample').css('background-image', 'url("' + url.replace(/_thumb/, '') + '")');
     });
     
+    // Clear image under pacifier
+    $('.btn-clear-design').click(function () {
+        $('#cyoSample').css('background-image', 'none');
+    });
+
+    // Wire up tabs to show/hide image divs
     $('#tabs li').click(function () {
         var tabNumber = $(this).attr('aria-controls').split('-')[1];
         console.log(tabNumber);
@@ -23,6 +30,17 @@
         $(this).addClass('ui-tabs-active').addClass('ui-state-active');
     });
 
+
+    // Set custom text as user types
+    $('#cyoCustomText').keyup(function () {
+        var text = $(this).val().replace(/\n/, "<br/>");
+        $('#cyoOverlay').html(text);
+    });
+
+    $('#cyoClearText').click(function () {
+        $('#cyoOverlay').html('');
+        $('#cyoCustomText').val('');
+    });
 
 
     //// Custom pattern menu
