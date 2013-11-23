64eefdae577af18c83fd0704d1af1008352f6b82
A. Diamond <andrew@7simplemachines.com>
2013-11-23 12:56:58 -0800

    CYO basic front-end is working

--- Presentation/Nop.Web/Scripts/Custom/cyo/cyo.js
+++ Presentation/Nop.Web/Scripts/Custom/cyo/cyo.js
@@ -7,26 +7,6 @@
     $('#font-size-slider a').css('left', '60px');
     setFontSize($('#font-size-slider a'));
 
-    // When user click while/pink/blue binky, load that image into the customizer on the left
-    $('.chooser.shields img').click(function () {
-        var bgImage = $('#cyoSample').css('background-image');
-        $('#cyoSample img').attr('src', $(this).attr('data-large-image'));
-        console.log(bgImage);
-        $('#cyoSample').css('background-image', bgImage);
-    });
-
-    // Load clicked image into pacifier
-    $('.chooser img').click(function () {
-        var url = $(this).attr('src');
-        //$('#cyoCustomPattern').attr('value', url);
-        $('#cyoSample').css('background-image', 'url("' + url.replace(/_thumb/, '') + '")');
-        return false;
-    });
-    
-    // Clear image under pacifier
-    $('.btn-clear-design').click(function () {
-        $('#cyoSample').css('background-image', 'none');
-    });
 
     // Wire up tabs to show/hide image divs
     $('#tabs li').click(function () {
@@ -41,16 +21,46 @@
     });
 
 
+    // Copy product size to hidden form when user changes it
+    $('input[name=size]').change(function () {
+        $('#cyoProductSize').val($(this).val());
+    });
+
+    // When user click while/pink/blue binky, load that image into the customizer on the left
+    $('.shields img').click(function () {
+        var bgImage = $('#cyoSample').css('background-image');
+        $('#cyoSample img').attr('src', $(this).attr('data-large-image'));
+        $('#cyoSample').css('background-image', bgImage);
+        // Copy product color to hidden form
+        $('#cyoProductColor').val($(this).attr('data-shield'));
+    });
+
+    // Load clicked image into pacifier
+    $('.chooser img').click(function () {
+        var url = $(this).attr('src');
+        $('#cyoSample').css('background-image', 'url("' + url.replace(/_thumb/, '') + '")');
+        $('#cyoImage').val(url);
+        return false;
+    });
+    
+    // Clear image under pacifier
+    $('.btn-clear-design').click(function () {
+        $('#cyoSample').css('background-image', 'none');
+        $('#cyoImage').val('');
+    });
+
     // Set custom text as user types
     $('#cyoCustomText').keyup(function () {
         var text = $(this).val().replace(/\n/, "<br/>");
         $('#cyoOverlay').html(text);
+        $('#cyoText').val(text);
     });
 
     // Clear custom text when user clicks Clear button
     $('#cyoClearText').click(function () {
         $('#cyoOverlay').html('');
         $('#cyoCustomText').val('');
+        $('#cyoText').val('');
     });
 
     // Custom font menu
@@ -60,12 +70,14 @@
         var font = $(this).css('font-family');
         $('#cyoCustomFont').attr('value', font);
         $('#cyoOverlay').css('font-family', font);
+        $('#cyoFontFamily').val(font.replace(/'/, ''));
     });
 
     // Allow font color input to set font color in overlay
     $('#cyoCustomColor').change(function () {
         var hexColor = '#' + document.getElementById('cyoCustomColor').color.toString();
         $('#cyoOverlay').css('color', hexColor);
+        $('#cyoFontColor').val(hexColor);
     });
 
     // When user clicks color wheel, focus color input so pop-up colorpicker appears.
@@ -81,7 +93,9 @@
 
     function setFontSize(sliderControl) {
         var position = parseInt(sliderControl.css('left'), 10);
-        $('#cyoOverlay').css('font-size', parseInt((position / 1.5), 10));
+        var fontSize = parseInt((position / 1.5), 10);
+        $('#cyoOverlay').css('font-size', fontSize);
+        $('#cyoFontSize').val(fontSize + 'px');
     }
 
     // Create Proof
