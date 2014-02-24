e15c5f1d8be072eef967d84633452b0b388171a6
A. Diamond <andrew@7simplemachines.com>
2014-02-24 14:19:38 -0800

    Added validation for wholesale fields.

--- Presentation/Nop.Web/Administration/Models/Customers/WholesalerModel.cs
+++ Presentation/Nop.Web/Administration/Models/Customers/WholesalerModel.cs
@@ -1,6 +1,7 @@
 ﻿using Nop.Web.Framework.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,25 @@ namespace Nop.Admin.Models.Customers
 {
     public class WholesalerModel : BaseNopEntityModel
     {
+        [Required]
         public string TaxId { get; set; }
+        [Required]
         public string WebsiteURL { get; set; }
+        [Required]
         public bool International { get; set; }
+        [Required]
         public string HowDidYouHear { get; set; }
+        [Required]
         public string YearsInBusiness { get; set; }
+        [Required]
         public string StoreFront { get; set; }
+        [Required]
         public string TypeOfStore { get; set; }
+        [Required]
         public string NameOfWebStore { get; set; }
+        [Required]
         public string AmazonSellerName { get; set; }
+        [Required]
         public bool AcceptedTerms { get; set; }
     }
 }
